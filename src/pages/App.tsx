86dfde21e8b943ea90e4cd5780230c782aa8228a
Wen Jie <wenjie@netizenexperience.com>
2024-08-12 22:43:25 +0800

    feat(ui): Added background color, button stylings, and game board styling

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,14 @@
-import "./App.module.css";
 import CustomButton from "../components/Button/Button";
 import Instructions from "../components/Instructions";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <>
       {/* <h1>Hello, Welcome to Connect 4</h1> */}
-      <div className="root">
+      <div className={styles.root}>
         <img alt="Connect 4 logo" src="src\assets\logo-removebg-preview.png" />
-        <div className="card">
+        <div className={styles.card}>
           <CustomButton label="Start Game" to="/game" />
           <Instructions label="Instructions and Rules" />
         </div>
